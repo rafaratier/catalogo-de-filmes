1ebb1cb6d4a7b6e4b62a4942777d3da52d7ccf15
rafaratier <rafaelratier@hotmail.com>
2024-10-20 21:36:19 -0300

    feat: read directors page

--- spec/system/genres/read_genres_spec.rb
+++ spec/system/genres/read_genres_spec.rb
@@ -4,6 +4,6 @@ describe 'User accesses the genres page' do
   it 'and can navigate to the page for adding a new movie genre' do
     visit genres_path
     click_link 'New Genre'
-    expect(page).to have_current_path('/genres/new')
+    expect(page).to have_current_path(new_genre_path)
   end
 end
